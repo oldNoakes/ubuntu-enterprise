93a66dbdc089f5e25797a18fa318faa472642668
noakes <old.noakes@gmail.com>
2017-12-05 11:12:34 +1100

    Moving CNTLM logging to a specific file

--- script/cntlm.sh
+++ script/cntlm.sh
@@ -4,6 +4,13 @@ set -eux
 apt-get -y install cntlm
 systemctl stop cntlm
 
+# Move CNTLM logs to a separate file via rsyslog
+mv /tmp/cntlm/syslog-cntlm.conf /etc/rsyslog.d/05-cntlm.conf
+systemctl restart rsyslog
+
+# Turn on verbose logging so we can see the details of requests in the log for debugging purposes
+echo 'DAEMON_OPTS="-v"' >> /etc/default/cntlm
+
 mv /tmp/cntlm/setup_proxy_password /usr/local/bin/setup_proxy_password
 chmod a+x /usr/local/bin/setup_proxy_password 
 
