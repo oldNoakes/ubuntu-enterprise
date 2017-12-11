e495e8bdd5ce570f3a93b3758aeba9f522ee7011
noakes <old.noakes@gmail.com>
2017-12-11 11:44:41 +1100

    Updating installation to use apt sources.list.d

--- script/docker.sh
+++ script/docker.sh
@@ -7,13 +7,12 @@ apt-get -y update
 echo "Ensure AUFS packages are installed for OverlayFS"
 apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
-echo "Required packages for setting up Docker repo"
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-
 echo "Install the docker repository"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable" # Artful Aardvark still does not have a stable Docker-ce release
+
+#echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+# Artful Aardvark still does not have a stable Docker-ce release
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"  > /etc/apt/sources.list.d/docker.list 
 
 echo "Installing docker-ce"
 apt-get -y update
