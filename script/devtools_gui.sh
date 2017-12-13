6546970b90999779bff3c902e379415331201607
noakes <old.noakes@gmail.com>
2017-12-13 13:02:21 +1100

    Adding gnome-terminal and firefox to GUI installation

--- script/devtools_gui.sh
+++ script/devtools_gui.sh
@@ -8,6 +8,13 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 apt-key add microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
+echo "Installing Visual Studio Code"
 apt-get update
 apt-get -y install libxss1
 apt-get -y install code
+
+echo "Installing Firefox"
+apt-get -y install firefox
+
+echo "Installing gnome-terminal"
+apt-get -y install gnome-terminal
